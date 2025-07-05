9e7318112e9c05dfe283e1f058a2bcf57528c383
Aiza Lee <lee.aizazia@gmail.com>
2025-07-05 23:24:40 +0800

    ResourceMono改回来了，原来的写法是正确的，在unity场景编辑器中创建实例，于start中注册到World中

--- Assets/NsEcsFrame/Unity/ResourceMono.cs
+++ Assets/NsEcsFrame/Unity/ResourceMono.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace NsEcsFrame.Unity {
 	/// <summary>
-	/// 在unity面板中直接调试Ecs中的Resource资源。
+	/// 在unity面板中直接调试Ecs中的Resource资源，会在初始化的时候覆盖world中注册的同类型资源。
 	/// </summary>
 	public abstract class ResourceMono<T> : MonoBehaviour where T : class, IResource {
 		[SerializeField] private T _resource;
@@ -13,11 +13,7 @@ namespace NsEcsFrame.Unity {
 				Debug.LogError($"ResourceMono<{typeof(T).Name}>: MainWorld is not initialized.");
 				return;
 			}
-			if (WorldBehaviour.MainWorld.HasResource<T>()) {
-				_resource = WorldBehaviour.MainWorld.GetResource<T>();
-			} else {
-				Debug.LogError($"ResourceMono<{typeof(T).Name}>: Resource not found in MainWorld.");
-			}
+			WorldBehaviour.MainWorld.InsertResource(_resource);
 		}
 	}
 }
