6884439f30c85ae9d22b5972f6e9a380051932dd
Aiza Lee <lee.aizazia@gmail.com>
2025-07-13 18:58:09 +0800

    迁移Light2DLayersMaskAccessExtension，处理2D灯光的

---
+++ Assets/GameLogic/Common/Utils/Light2DLayersMaskAccessExtension.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using UnityEngine.Rendering.Universal;
+
+namespace GameLogic.Common.Utils {
+	// note: Be careful! If during development the Unity team changes the name of the "m_ApplyToSortingLayers" variable
+	// note: , here it will also need to be changed.
+	public static class Light2DLayersMaskAccessExtension {
+		public static int[] GetLayers(this Light2D light) {
+			FieldInfo targetSortingLayersField = typeof(Light2D).GetField("m_ApplyToSortingLayers",
+																	   BindingFlags.NonPublic |
+																	   BindingFlags.Instance);
+			int[] mask = targetSortingLayersField.GetValue(light) as int[];
+			return mask;
+		}
+		public static void SetLayers(this Light2D light, params int[] mask) {
+			FieldInfo targetSortingLayersField = typeof(Light2D).GetField("m_ApplyToSortingLayers",
+																	   BindingFlags.NonPublic |
+																	   BindingFlags.Instance);
+			targetSortingLayersField.SetValue(light, mask);
+		}
+	}
+}
